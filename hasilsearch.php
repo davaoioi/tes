69f57b8069e6a065202e891065273082b6fd9d48
fitra <cahrizki.rf@gmail.com>
2020-12-22 12:24:01 +0700

    change to mysqli

--- hasilsearch.php
+++ hasilsearch.php
@@ -3,22 +3,29 @@
 		<option value="nama">nama</option>
 		<option value="email">email</option>
 	</select>
-Masukkan kata yang anda cari
-<input type ="text" type ="text" name="cari">
-<input type ="submit" value="cari" >
+	Masukkan kata yang anda cari
+	<input type="text" type="text" name="cari">
+	<input type="submit" value="cari">
 </form>
 <?php
-	$kolom= $_POST['kolom'];
-	$cari=$_POST['cari'];
-	$conn = mysql_connect("localhost","root","");
-	mysql_select_db("latihan",$conn);
-	$hasil = mysql_query("SELECT * FROM bukutamu WHERE $kolom LIKE '%s%%'",$conn);
-	$jumlah = mysql_num_rows($hasil) or die(mysql_error());
-	echo "<br>";
-	echo "Ditemukan: $jumlah"; echo "<br>";
+$kolom = $_POST['kolom'];
+$cari = $_POST['cari'];
+$conn = mysqli_connect("localhost", "root", "", "latihan");
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+$stmt = $conn->prepare("SELECT * FROM bukutamu WHERE $kolom LIKE ?");
+$cari = "%$cari%";
+$stmt->bind_param("s", $cari);
+$stmt->execute();
+$hasil = $stmt->get_result();
+$jumlah = $hasil->num_rows or die($conn->connect_error);
+echo "<br>";
+echo "Ditemukan: $jumlah";
+echo "<br>";
 
-	 
-	echo '<table border="1">
+
+echo '<table border="1">
 		<thead>
 			<tr>
 				<th width="200">NAMA</th>
@@ -27,17 +34,16 @@ Masukkan kata yang anda cari
 			</tr>
 		</thead>
 	<tbody>';
-	while ($row = mysql_fetch_array($hasil));
-	{
+while ($row = $hasil->fetch_array()) {
 	echo '<tr>
-		<td>'.$row[0].'</td>
-		<td>'.$row[1].'</td>
+		<td>' . $row[0] . '</td>
+		<td>' . $row[1] . '</td>
 		 
-		<td>'.$row[2].'</td>
+		<td>' . $row[2] . '</td>
 	</tr>';
-	}
-	echo '
+}
+echo '
 	</tbody>
 	</table>';
-	?><br>
-	<a	href="show.php"><input	type="button"	name="show"	id="button" value="tampilkan buku tamu"></a>
+?><br>
+<a href="show.php"><input type="button" name="show" id="button" value="tampilkan buku tamu"></a>
